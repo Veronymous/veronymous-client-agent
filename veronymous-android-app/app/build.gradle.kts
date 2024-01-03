6489edd9f77f8088b9f4719aba304778400485cd
Noah Bouma <noah.bouma@gmail.com>
2024-01-03 08:36:16 -0500

    android-app: Configured obsfucation for builds

--- veronymous-android-app/app/build.gradle.kts
+++ veronymous-android-app/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "io.veronymous.vpn.android.app"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -23,7 +23,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
